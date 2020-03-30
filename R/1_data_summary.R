f8d7c9cfd8669d0b18dd6d7dcee48d81ba41fc07
krietsch <jkrietsch@orn.mpg.de>
2020-03-30 09:27:26 +0200

    data summary for parentage analysis, initiation date method, rate of polyandry & renesting

---
+++ R/1_data_summary.R
@@ -0,0 +1,257 @@
+#========================================================================================================================
+# Nest data summary
+#========================================================================================================================
+
+# Summary
+# 1. Captures on and off plot
+# 2. Incubation length & initiation date method
+# 3. Rate of polyandry & renesting
+
+# Packages
+sapply( c('data.table', 'magrittr', 'sdb', 'ggplot2', 'sf', 'auksRuak'),
+        require, character.only = TRUE)
+
+# Projection
+PROJ = '+proj=laea +lat_0=90 +lon_0=-156.653428 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0 '
+
+# Data
+con = dbcon('jkrietsch', db = 'REPHatBARROW')  
+dc = dbq(con, 'select * FROM CAPTURES')
+d = dbq(con, 'select * FROM NESTS')
+dp = dbq(con, 'select * FROM PATERNITY')
+DBI::dbDisconnect(con)
+
+#------------------------------------------------------------------------------------------------------------------------
+# 0. Prepare data for analysis
+#------------------------------------------------------------------------------------------------------------------------
+
+# Change projection
+ds = d[is.na(lon)] # seperate data without position
+d = d[!is.na(lon)]
+st_transform_DT(d)
+
+# Assign locations in the study area 
+point_over_poly_DT(d, poly = study_site, buffer = 10)
+setnames(d, 'poly_overlap', 'study_site')
+
+# merge with data without position
+ds[, study_site := NA]
+d = rbind(d, ds)
+
+# nestID
+d[, nestID := paste0(nest, '_', substr(year_, 3,4 ))]
+dp[, nestID := paste0(nest, '_', substr(year_, 3,4 ))]
+
+# assign datetime format
+d[, found_datetime := as.POSIXct(found_datetime)]
+d[, collected_datetime := as.POSIXct(collected_datetime)]
+d[, initiation := as.POSIXct(initiation)]
+d[, initiation_y := as.POSIXct(format(initiation, format = '%m-%d %H:%M:%S'), format = '%m-%d %H:%M:%S')]
+d[, est_hatching_datetime := as.POSIXct(est_hatching_datetime)]
+d[, hatching_datetime := as.POSIXct(hatching_datetime)]
+
+# nests with paternity data
+# EPY father 
+dp[EPY == 1, .N, by = nestID] # more than 1 EPY father? No
+dp[EPY == 1, EPY_father := IDfather]
+dps = dp[, .(EPY = sum(EPY), N = .N, EPY_father = min(EPY_father, na.rm = TRUE)), by = .(nestID, year_)]
+dps[, anyEPY := ifelse(EPY > 0, 1, 0)]
+dps[is.infinite(EPY_father), EPY_father := NA]
+dps[!is.na(EPY_father)] %>% nrow # known EPY fathers
+
+# merge with nests
+d = merge(d, dps[, .(nestID, N_parentage = N, N_EPY = EPY, anyEPY, EPY_father)], by = 'nestID', all.x = TRUE)
+
+# assign all nests with parentage data
+d[, parentage := ifelse(!is.na(N_parentage), TRUE, FALSE)]
+d[, any_parentage_year := any(parentage == TRUE), by = year_]
+
+# assign nests on plot (Rick's plot)
+d[external == 1, study_site := ifelse(plot %like% 'brw', TRUE, FALSE)]
+d[plot == 'brwfwl', study_site := FALSE]
+
+#------------------------------------------------------------------------------------------------------------------------
+# 1. Nests data avaiable
+#------------------------------------------------------------------------------------------------------------------------
+
+# exclude off-plot nests without parentage data
+d0 = d[external == 0 & !(study_site == FALSE & parentage == FALSE)]
+d1 = d[external == 1 & parentage == TRUE]
+
+#------------------------------------------------------------------------------------------------------------------------
+# 1. Captures on and off plot
+#------------------------------------------------------------------------------------------------------------------------
+
+# Change projection
+ds = dc[is.na(lon)] # seperate data without position
+dc = dc[!is.na(lon)]
+st_transform_DT(dc)
+
+point_over_poly_DT(dc, poly = study_site, buffer = 60) # buffer including birds followed flying off plot
+setnames(dc, 'poly_overlap', 'study_site')
+
+# merge with data without position
+ds[, study_site := FALSE]
+dc = rbind(dc, ds)
+
+# check data
+bm = create_bm(dc[study_site == TRUE], buffer = 500)
+
+bm +
+  geom_point(data = dc, aes(lon, lat, color = study_site))
+
+# any without metal band?
+dc[is.na(ID)]
+
+# assign first capture
+dc[, caught_time := as.POSIXct(caught_time)]
+setorder(dc, year_, caught_time)
+dc[, capture_id := seq_len(.N), by = ID]
+dc[, .N, capture_id]
+
+# banded each year on and off plot by us
+ds = dc[external == 0 & capture_id == 1 & year_ > 2016]
+
+ds[, .N, .(year_, study_site)]
+
+#------------------------------------------------------------------------------------------------------------------------
+# 2. Incubation length & initiation date method
+#------------------------------------------------------------------------------------------------------------------------
+
+# incubation period in incubator
+d[, found_incomplete := initial_clutch_size < clutch_size]
+d[, inc_period := difftime(hatching_datetime, c(initiation + clutch_size * 3600*24 - 3600*24), units = 'days') %>% as.numeric]
+
+ds = d[!is.na(inc_period) & found_incomplete == TRUE]
+mean(ds[external == 0]$inc_period, na.rm = TRUE) # 17.2 days is based on eggs
+nrow(ds[external == 0]) 
+
+# not in incubator
+mean(ds[external == 1]$inc_period, na.rm = TRUE)
+nrow(ds[external == 1])
+mean(ds[external == 1 & inc_period < 25]$inc_period, na.rm = TRUE) # excluding outliers
+
+ggplot(data = ds[external == 0]) +
+  geom_point(aes(y = inc_period, x = initiation_y, color = as.character(year_))) +
+  geom_smooth(aes(y = inc_period, x = initiation_y), method = 'lm')
+
+ggplot(data = ds[external == 1 & inc_period < 25]) +
+  geom_point(aes(y = inc_period, x = initiation_y, color = as.character(year_))) +
+  geom_smooth(aes(y = inc_period, x = initiation_y), method = 'lm')
+
+ggplot(data = ds[inc_period < 25]) +
+  geom_point(aes(y = inc_period, x = initiation_y, color = as.character(year_))) +
+  geom_smooth(aes(y = inc_period, x = initiation_y), method = 'lm')
+
+# comparision natural vs. incubator
+ggplot(data = ds) +
+  geom_boxplot(aes(y = inc_period, x = as.character(external))) 
+
+ggplot(data = ds[year_ > 2016]) +
+  geom_boxplot(aes(y = inc_period, x = as.character(external))) 
+
+# initiation date methods
+d[found_incomplete == TRUE, initiation_method := 'found_incomplete']
+d[is.na(initiation_method) & !is.na(hatching_datetime), initiation_method := 'hatching_datetime']
+d[is.na(initiation_method) & !is.na(est_hatching_datetime), initiation_method := 'est_hatching_datetime']
+
+# check NA
+d[external == 0 & is.na(initiation_method), .(year_, nest, initiation_y)]
+
+# d[is.na(initiation_method) & !is.na(initiation), initiation_method := 'found_incomplete']
+
+ds = d[external == 1 & parentage == TRUE, .N, initiation_method]
+ds[, N_nests := nrow(d[external == 1 & parentage == TRUE])]
+ds[, initiation_method_percent := N / N_nests * 100]
+ds
+
+
+#------------------------------------------------------------------------------------------------------------------------
+# 3. Rate of polyandry & renesting
+#------------------------------------------------------------------------------------------------------------------------
+
+setorder(d, year_, initiation)
+
+# first and second clutches by females
+d[, N_female_clutch := .N, by = female_id]
+d[, female_clutch := seq_len(.N), by = .(female_id, year_)]
+d[is.na(female_id), female_clutch := 1]
+d[!is.na(female_id), N_female_clutch := max(female_clutch), by = .(female_id, year_)]
+d[is.na(female_id), N_female_clutch := 1]
+d[, .N, by = .(year_, female_clutch)]
+d[, .N, by = .(female_clutch, external)]
+
+# polyandrous clutches (second clutch with different partner)
+ID2c = d[female_clutch == 2]$female_id_year
+dx = d[female_id_year %in% ID2c]
+dx[, .N, by = .(female_clutch, anyEPY)]
+dx[, female_clutch := as.factor(female_clutch)]
+
+dr = merge(dx[female_clutch == 1, .(year1 = year_, nestID1 = nestID, female_id_year, m1 = male_id, anyEPY1 = anyEPY, 
+                                    mc1 = male_clutch, ss1 = study_site)], 
+           dx[female_clutch == 2, .(year2 = year_, nestID2 = nestID, female_id_year, m2 = male_id, anyEPY2 = anyEPY, 
+                                    mc2 = male_clutch, ss2 = study_site)], 
+           by = 'female_id_year', all = TRUE)
+
+dr[, same_male := m1 == m2]
+dr[is.na(same_male), same_male := FALSE]
+dr[, both_study_site := ss1 == ss2]
+setorder(dr, female_id_year)
+dr
+
+# polyandrous females
+dr = dr[same_male == FALSE, .(female_id_year, polyandrous = TRUE, polyandry_study_site = both_study_site)]
+d = merge(d, dr, by = 'female_id_year', all.x = TRUE)
+
+# males renesting
+d[, male_id_year := paste0(male_id, '_', substr(year_, 3,4 ))]
+d[, N_male_clutch := .N, by = male_id]
+d[, male_clutch := seq_len(.N), by = male_id_year]
+d[is.na(male_id), male_clutch := 1]
+d[!is.na(male_id), N_male_clutch := max(male_clutch), by = male_id_year]
+d[is.na(male_id), N_male_clutch := 1]
+d[, .N, by = .(year_, male_clutch)]
+d[, .N, by = .(male_clutch, external)]
+
+# males renesting with same or different partner
+ID2c = d[male_clutch == 2]$male_id_year
+dx = d[male_id_year %in% ID2c]
+dx[, .N, by = .(male_clutch, anyEPY)]
+dx[, male_clutch := as.factor(male_clutch)]
+
+dr = merge(dx[male_clutch == 1, .(year1 = year_, nestID1 = nestID, male_id_year, f1 = female_id, anyEPY1 = anyEPY, 
+                                  mfc1 = male_clutch, ss1 = study_site)], 
+           dx[male_clutch == 2, .(year2 = year_, nestID2 = nestID, male_id_year, f2 = female_id, anyEPY2 = anyEPY, 
+                                  fc2 = male_clutch, ss2 = study_site)], 
+           by = 'male_id_year', all = TRUE)
+
+dr[, same_female := f1 == f2]
+dr[is.na(same_female), same_female := FALSE]
+dr[, both_study_site := ss1 == ss2]
+setorder(dr, male_id_year)
+dr
+
+# polyandrous females
+dr = dr[, .(male_id_year, renesting_male = TRUE, same_female, renesting_study_site = both_study_site)]
+d = merge(d, dr, by = 'male_id_year', all.x = TRUE)
+
+
+# number of males assigned and females assigned with ID on plot
+ds = d[external == 0 & study_site == TRUE]
+ds = d[external == 0]
+
+ds[is.na(male_id) & parentage == TRUE]
+
+ds[is.na(female_id)]
+
+ds[male_assigned == 3]
+ds[female_assigned == 3]
+
+
+
+
+
+
+
+
+
