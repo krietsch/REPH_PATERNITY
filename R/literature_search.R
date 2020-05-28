0395dd79aa1004f8e2ecc7c7100343978cf9c1fd
krietsch <jkrietsch@orn.mpg.de>
2020-05-28 09:19:30 +0200

    added Brouwer dataset

--- R/literature_search.R
+++ R/literature_search.R
@@ -14,10 +14,32 @@ dsp = df[family %in% c('scolopacidae', 'charadriidae', 'burhinidae', 'pedionomid
                        'rostratulidae', 'jacanidae')]
 # all data
 d = readRDS('./DATA/promiscuity.rds')
-d = readRDS('./DATA/promiscuity_v2.rds')
+d2 = readRDS('./DATA/promiscuity_v2.rds')
 
 d = d[scinam %in% dsp$scinam]
 d2 = d2[scinam %in% dsp$scinam]
+
+# merge with shorebird data
+d = merge(d, ds, by = 'scinam', all.x = TRUE)
+d2 = merge(d2, ds, by = 'scinam', all.x = TRUE)
+
+write.csv2(d, './DATA/AVES_v1.csv')
+write.csv2(d2, './DATA/AVES_v2.csv')
+
+
+# open Brouwer_et_al_ESM
+d = read.csv2('./DATA/Brouwer_et_al_ESM.csv') %>% data.table
+d[, Scientific.name := tolower(Scientific.name)]
+
+d = d[Scientific.name %in% dsp$scinam]
+
+write.csv(d, './DATA/Brouwer_et_al_ESM_shorebirds.csv')
+
+
+
+
+
+
 
 
   
