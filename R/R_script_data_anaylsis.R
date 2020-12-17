bdf536588222865fbafbd9054e3cf77874e54718
krietsch <jkrietsch@orn.mpg.de>
2020-12-17 10:41:12 +0100

    started script for publication

---
+++ R/R_script_data_anaylsis.R
@@ -0,0 +1,69 @@
+#========================================================================================================================
+# Extract all for the parentage study relevant data & prepare them for analysis
+#========================================================================================================================
+
+# Summary
+# 0. Prepare data for analysis
+
+# Packages
+sapply( c('data.table', 'magrittr', 'sdb', 'sf', 'auksRuak'),
+        function(x) suppressPackageStartupMessages(require(x , character.only = TRUE, quietly = TRUE)))
+
+# Projection
+PROJ = '+proj=laea +lat_0=90 +lon_0=-156.653428 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0 '
+
+#------------------------------------------------------------------------------------------------------------------------
+# METHODS
+#------------------------------------------------------------------------------------------------------------------------
+
+
+#------------------------------------------------------------------------------------------------------------------------
+# Table 1 - Samples available 
+#------------------------------------------------------------------------------------------------------------------------
+
+# Number of captures 
+
+
+# Initiation date method
+
+
+### Parentage analysis 
+
+# mean clutch size
+
+# N offspring sampled
+
+# undeveloped eggs
+
+
+
+
+
+#------------------------------------------------------------------------------------------------------------------------
+# RESULTS
+#------------------------------------------------------------------------------------------------------------------------
+
+
+### Frequency of extra-pair paternity, social polyandry and renesting -----
+
+# EPY in nests and eggs
+
+# EPY difference between years?
+
+# Figure 2a
+
+# Number of EPY in each clutch
+
+# rates of social polyandry
+
+# Figure 2b
+
+# rates pf renesting
+
+# correlation EPY ~ season lenght or polyandry
+
+
+### Extra-pair paternity and clutch order -----
+
+
+
