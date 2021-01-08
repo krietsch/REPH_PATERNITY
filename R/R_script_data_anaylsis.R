1a4b3735738e1854df48924031103fa28ee2788c
krietsch <jkrietsch@orn.mpg.de>
2021-01-08 15:54:14 +0100

    started to link paper and analysis

--- R/R_script_data_anaylsis.R
+++ R/R_script_data_anaylsis.R
@@ -16,13 +16,56 @@ PROJ = '+proj=laea +lat_0=90 +lon_0=-156.653428 +x_0=0 +y_0=0 +datum=WGS84 +unit
 # METHODS
 #------------------------------------------------------------------------------------------------------------------------
 
+# load data
+d = read.table('./DATA/NESTS.txt', sep = '\t',header = TRUE) %>% data.table
+
+# Intensive study site
+st_area(study_site) %>% as.numeric/ 1000000 # in km²
+
+# N nests with parentage
+ds = d[!is.na(data_type), .(N_nests = .N), by =  data_type]
+ds2 = d[parentage == TRUE, .(N_parentage = .N), by =  data_type]
+ds = merge(ds, ds2, by = c('data_type'), all.x = TRUE)
+ds[, N_parentage := paste0(round(N_parentage / N_nests * 100, 0), '% (', N_parentage, '/', N_nests, ')')]
+ds
 
 #------------------------------------------------------------------------------------------------------------------------
 # Table 1 - Samples available 
 #------------------------------------------------------------------------------------------------------------------------
 
+
+
+
+
+
+
+
+
+
+
+
+# load data
+d = read.table('./DATA/CAPTURES.txt', sep = '\t',header = TRUE) %>% data.table
+
+
+
+
+bm = create_bm(d, buffer = 500)
+bm +
+  geom_sf(data = study_site, color = 'red') +
+  coord_sf(expand = FALSE) +
+  geom_point(data = d, aes(lon, lat, color = data_type))
+
+
 # Number of captures 
 
+
+
+
+
+
+# banded each year on and off plot by us
+ds = d[external == 0 & capture_id == 1 & year_ > 2016]
 
 # Initiation date method
 
