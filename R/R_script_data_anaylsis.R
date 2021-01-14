d8b28028e26364c9aa613212a5659322e4d56c94
krietsch <jkrietsch@orn.mpg.de>
2021-01-14 15:31:09 +0100

    included figure 2

--- R/R_script_data_anaylsis.R
+++ R/R_script_data_anaylsis.R
@@ -6,7 +6,7 @@
 # 0. Prepare data for analysis
 
 # Packages
-sapply( c('data.table', 'magrittr', 'sdb', 'sf', 'auksRuak'),
+sapply( c('data.table', 'magrittr', 'sdb', 'sf', 'auksRuak', 'ggplot2', 'ggnewscale'),
         function(x) suppressPackageStartupMessages(require(x , character.only = TRUE, quietly = TRUE)))
 
 # Projection
@@ -115,12 +115,12 @@ bm +
 
 
 ds = d[data_type == 'intensive_study' & parentage == TRUE, .N, initiation_method]
-ds[, N_nests := nrow(d[study_site == TRUE & parentage == TRUE])]
+ds[, N_nests := nrow(d[data_type == 'study_site' & parentage == TRUE])]
 ds[, initiation_method_percent := N / N_nests * 100]
 ds
 
 # age when found
-ds = d[study_site == TRUE]
+ds = d[data_type == 'study_site' ]
 ds[, complete := initiation + clutch_size*86400]
 
 ds[, age_found := difftime(found_datetime, initiation, units = 'days') %>% as.numeric]
@@ -296,17 +296,238 @@ p1 =
 p1
 
 
-ggplot(data = ds[study_site == FALSE], aes(type, x, fill = year_, label = sample_size)) +
+ds[, sample_size_N := paste0(n, '/', N)]
+ds[year_ == '2019' & type %in% c('polyandry', 'renesting'), sample_size_N := paste0(sample_size_N, '*')]
+
+p1 = 
+  ggplot(data = ds[study_site == FALSE], aes(year_, x, fill = type, label = sample_size_N)) +
   geom_bar(stat = "identity", position = 'dodge', width = 0.9) +
-  geom_text(position = position_dodge(width = 0.9), size = ls, vjust = -0.5, hjust = 0.5) +
-  scale_fill_grey(start = 0.8, end = 0.3, labels = c('2017', '2018', '2019')) +
+  geom_text(position = position_dodge(width = 0.9), size = lsa, hjust = -0.1, angle = 90) +
+  # scale_fill_manual(values = c('grey85', 'grey50','firebrick4', '#33638DFF'), 
+  #                   labels = c('EPY', 'nests with EPY', 'polyandrous females', 'renesting males')) +
+  scale_fill_grey(labels = c('EPY', 'nests with EPY', 'polyandrous females', 'renesting males')) +
   scale_y_continuous(breaks = c(0, 5, 10, 15), limits = c(0, 16), expand = c(0, 0)) +
-  geom_text(aes(Inf, Inf, label = 'b'), vjust = vjust_label, hjust = hjust_,  size = lsa) +
-  xlab('Type') + ylab('Percentage') + 
+  # geom_text(aes(Inf, Inf, label = 'b'), vjust = vjust_label, hjust = hjust_,  size = lsa) +
+  xlab('Year') + ylab('Percentage') + 
   theme_classic(base_size = bs) +
   theme(legend.position = c(0.285, 0.82), legend.title = element_blank(), legend.background = element_rect(fill = alpha('white', 0)))
+p1
 
 
+
+#------------------------------------------------------------------------------------------------------------------------
+# 2. Nests on plot
+#------------------------------------------------------------------------------------------------------------------------
+
+d[, initiation := as.POSIXct(initiation)]
+d[, initiation_y := as.POSIXct(format(initiation, format = '%m-%d %H:%M:%S'), format = '%m-%d %H:%M:%S')]
+
+ds = d[data_type == 'study_site' & !is.na(initiation_y)]
+
+ds[, initiation_date := as.Date(initiation)]
+y = ds[, .N, by = .(year_, initiation_date)]
+y[, max(N), by = year_]
+
+dss = ds[, .(median = median(initiation_y), q25 = quantile(initiation_y, probs = c(0.25)), 
+             q75 = quantile(initiation_y, probs = c(0.75)), .N, max = max(initiation_y)), by = year_]
+
+
+p2 = 
+  ggplot(data = ds) +
+  geom_violin(aes(as.character(year_), initiation_y), show.legend = FALSE, fill = 'grey85') +
+  geom_point(data = dss, aes(as.character(year_), median), size = 2) +
+  geom_linerange(data = dss, aes(x = as.character(year_), ymin = q75, ymax = q25), size = 0.5) +
+  geom_point(data = ds[year_ == 2017 & initiation_y < as.POSIXct('2020-06-16')], aes(as.character(year_), initiation_y), 
+             shape = 4, size = 1, stroke = 1.3) +
+  geom_text(data = dss, aes(as.character(year_), as.POSIXct('2021-07-02 11:03:00'), label = N), vjust = 0, size = ls) +
+  scale_y_datetime(breaks = c(as.POSIXct(c('2021-06-07', '2021-06-14', '2021-06-21', '2021-06-28'))), 
+                   labels = c('7', '14', '21', '28')) +
+  geom_text(aes(Inf, as.POSIXct(c('2021-07-02')), label = 'c'), vjust = vjust_label - 1.5, hjust = hjust_,  size = lsa) +
+  xlab('Year') + ylab('Clutch initiation date (June)') + 
+  theme_classic(base_size = bs)
+p2
+
+
+
+
+
+
+#------------------------------------------------------------------------------------------------------------------------
+# 3. Timing of multiple clutches vs. single clutches
+#------------------------------------------------------------------------------------------------------------------------
+
+# assign clutch identity
+d[female_clutch == 3, clutch_identity := 'third']
+d[female_clutch == 2, clutch_identity := 'second']
+d[female_clutch == 1 & N_female_clutch > 1, clutch_identity := 'first']
+d[female_clutch == 1 & N_female_clutch < 2, clutch_identity := 'one']
+d[clutch_identity == 'one' & anyEPY == FALSE, clutch_identity := 'one_noEPY']
+d[clutch_identity == 'one' & anyEPY == TRUE, clutch_identity := 'one_EPY']
+d[is.na(clutch_identity)]
+d[, .N, by = clutch_identity]
+
+# factor order
+d[, clutch_identity := factor(clutch_identity, levels = c('one_noEPY', 'one_EPY', 'first', 'second', 'third'))]
+
+# subset all multiple clutches or in study site
+ds = d[!is.na(initiation)]
+ds = ds[data_type == 'study_site' & !(clutch_identity %in% c('first', 'second', 'third')) | clutch_identity %in% c('first', 'second', 'third')]
+ds[, anyEPY := as.character(anyEPY)]
+
+ds[, mean_initiation := mean(initiation, na.rm = TRUE), by = year_]
+ds[, initiation_st := difftime(initiation, mean_initiation, units = 'days') %>% as.numeric]
+
+ds = ds[!is.na(anyEPY)]
+
+ds[is.na(renesting_male), renesting_male := FALSE]
+ds[, any_renesting := any(renesting_male == TRUE), by = female_id_year]
+ds[any_renesting == FALSE, next_clutch := 'polyandrous']
+ds[any_renesting == TRUE, next_clutch := 'renesting']
+
+# add first and second clutch of females with three clutches again (otherwise linetype does not work)
+ds[clutch_identity == 'third']
+dss = ds[female_id %in% c(270170935, 19222)]
+dss[, female_id_year_NA := paste0(female_id_year, '2')]
+
+ds = rbind(ds, dss)
+ds[!(clutch_identity %in% c('first', 'second', 'third')), clutch_identity := 'single']
+
+# correct females with three clutches
+ds = ds[!(female_id_year_NA %in% c('270170935_19', '19222_19') & clutch_identity == 'third')]
+ds[female_id_year_NA %in% c('270170935_19', '19222_19'), next_clutch := 'polyandrous']
+
+# factor order
+ds[, clutch_identity := factor(clutch_identity, levels = c('single', 'first', 'second', 'third'))]
+ds[, anyEPY := factor(anyEPY, levels = c('0', '1'))]
+
+
+theme_classic_edit = function (base_size = 11, base_family = "", base_line_size = base_size/22, 
+                               base_rect_size = base_size/22, lp = c(0.8, 0.2)) 
+{
+  theme_bw(base_size = base_size, base_family = base_family, 
+           base_line_size = base_line_size, base_rect_size = base_rect_size) %+replace% 
+    theme(panel.border = element_blank(), panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank(), 
+          axis.line = element_line(colour = "black", size = rel(1)), legend.key = element_blank(), 
+          strip.background = element_rect(fill = "white", colour = "black", size = rel(2)), complete = TRUE,
+          legend.position = lp)
+}
+
+ds[anyEPY == '1', .N, clutch_identity]
+ds[, .N, clutch_identity]
+
+ds[clutch_identity == 'first' & next_clutch == 'polyandrous']
+ds[clutch_identity == 'first' & next_clutch == 'renesting']
+
+dss = data.table(clutch_identity = c('single', 'first', 'second', 'third'),
+                 sample_size = c('138', '15', '15', '2'))
+
+p3 = 
+  ggplot() +
+  geom_hline(yintercept = 0, color = 'grey70') +
+  geom_boxplot(data = ds, aes(clutch_identity, initiation_st), fill = 'grey85', outlier.alpha = 0) +
+  geom_line(data = ds, aes(clutch_identity, initiation_st, group = female_id_year_NA, linetype = next_clutch), size = 0.3) +
+  geom_point(data = ds[clutch_identity != 'single' & anyEPY == '0'], aes(clutch_identity, initiation_st, fill = anyEPY),
+             shape = 21, size = 1) +
+  geom_point(data = ds[clutch_identity != 'single' & anyEPY == '1'], aes(clutch_identity, initiation_st, fill = anyEPY),
+             shape = 21, size = 1) +
+  geom_jitter(data = ds[clutch_identity == 'single'], aes(clutch_identity, initiation_st, fill = anyEPY), 
+              shape = 21, size = ps, height = 0, width = 0.3) +
+  scale_fill_manual(values = c('white', 'black'), name = NULL, labels = c('no EPY', 'EPY'), guide = FALSE) +
+  scale_linetype_manual(values = c('solid', 'dotted'), name = NULL) +
+  scale_x_discrete(labels = c('Single', 'First', 'Second', 'Third')) +
+  scale_y_continuous(breaks = c(-14, -7, 0, 7, 14), limits = c(-18, 18), expand = c(0, 0)) +
+  geom_text(aes(Inf, Inf, label = 'd'), vjust = vjust_label, hjust = hjust_,  size = lsa) +
+  xlab('Clutch type') + ylab('Clutch initiation date (standardized)') +
+  geom_text(data = dss, aes(clutch_identity, Inf, label = sample_size), vjust = vjust_, size = ls) +
+  theme_classic_edit(base_size = bs, lp = c(0.85, 0.08)) +
+  theme(legend.background = element_rect(fill = alpha('white', 0)),
+        legend.key.size = unit(0.5, 'lines'))
+p3
+
+
+# some descriptive statisic about the timing
+
+# exclude double again
+ds = ds[!(female_id_year_NA %in% c('270170935_19', '19222_19'))]
+ds[, .N, clutch_identity]
+
+# initiation first clutch compared to single clutch
+ds[clutch_identity == 'single', mean(initiation_st)] - ds[clutch_identity == 'first', mean(initiation_st)] 
+
+# second clutches compared to single
+ds[clutch_identity == 'single', mean(initiation_st)] - ds[clutch_identity == 'second', mean(initiation_st)] 
+ds[clutch_identity == 'single', mean(initiation_st)] - ds[clutch_identity == 'second' & polyandrous == TRUE, mean(initiation_st)] 
+ds[clutch_identity == 'single', mean(initiation_st)] - ds[clutch_identity == 'second' & polyandrous != TRUE, mean(initiation_st)] 
+
+ds[clutch_identity == 'third', .(initiation_st)] 
+
+# other timings see polyandry / renesting above
+
+
+#------------------------------------------------------------------------------------------------------------------------
+# 4. EPY and timing 
+#------------------------------------------------------------------------------------------------------------------------
+
+# subset all multiple clutches or in study site
+ds = d[!is.na(initiation) & !is.na(anyEPY)]
+ds = ds[, .(year_, nestID, initiation_y, anyEPY, data_type)]
+ds[data_type == 'study_site', data_type := 'Intensive study']
+ds[data_type != 'Intensive study', data_type := 'Other data']
+
+# load Dale et al. data
+dale = read.csv2('./DATA/Dale_EPP.csv') %>% data.table
+dale[, initiation_y := as.POSIXct(as.Date(initiation_doy, origin = '1993-01-01'))]
+
+ds = rbind(ds, dale[, .(year_ = YEAR_, nestID, initiation_y, anyEPY, data_type = 'Dale et al.')])
+
+
+ds[, mean_initiation := mean(initiation_y, na.rm = TRUE), by = year_]
+ds[, initiation_st := difftime(initiation_y, mean_initiation, units = 'days') %>% as.numeric]
+ds[, anyEPY := as.character(anyEPY)]
+ds[, study_site := as.character(study_site)]
+
+dss = ds[, .(median = median(initiation_st), q25 = quantile(initiation_st, probs = c(0.25)), 
+             q75 = quantile(initiation_st, probs = c(0.75)), .N), by = .(data_type, anyEPY)]
+
+dss2 = data.table(data_type = c(rep('Intensive study', 2), rep('Other data', 2), rep('Dale et al.', 2)),
+                  sample_size = c('16', '149', '21', '146', '6', '12'),
+                  anyEPY = c('1', '0', '1', '0', '1', '0'))
+
+# factor order
+ds[, data_type := factor(data_type, levels = c('Intensive study', 'Other data', 'Dale et al.'))]
+ds[, anyEPY := factor(anyEPY, levels = c('1', '0'))]
+
+dss2[, anyEPY := factor(anyEPY, levels = c('1', '0'))]
+dss2[, data_type := factor(data_type, levels = c('Intensive study', 'Other data', 'Dale et al.'))]
+
+
+p4 = 
+  ggplot(data = ds, aes(data_type, initiation_st, fill = anyEPY)) +
+  geom_hline(yintercept = 0, color = 'grey70') +
+  geom_boxplot(position = position_dodge(width = 0.9), outlier.alpha = 0, show.legend = FALSE) +
+  scale_fill_manual(values = c('#818181', 'white'), labels = c('0', '1')) +
+  new_scale_fill() +
+  geom_point(data = ds, aes(data_type, initiation_st, fill = anyEPY), shape = 21, size = ps,
+             position = position_jitterdodge(jitter.width = 0.6, jitter.height = 0, dodge.width = 0.9)) +
+  scale_fill_manual(values = c('black', 'white'), name = NULL, labels = c('EPY', 'no EPY')) +
+  scale_y_continuous(breaks = c(-14, -7, 0, 7, 14), limits = c(-18, 18), expand = c(0, 0)) +
+  geom_text(data = dss2, aes(data_type, Inf, group = anyEPY, label = sample_size), position = position_dodge(width = 0.9), vjust = vjust_, size = ls) +
+  geom_text(aes(Inf, Inf, label = 'e'), vjust = vjust_label, hjust = hjust_,  size = lsa) +
+  xlab('Data source') + ylab('Clutch initiation date (standardized)') + 
+  theme_classic_edit(base_size = bs, lp = c(0.89, 0.08)) +
+  theme(legend.background = element_rect(fill = alpha('white', 0)), 
+        legend.key.size = unit(0.5, 'lines'))
+
+
+p4
+
+# statistics
+fm = glm(anyEPY ~ initiation_st, data = ds[data_type == 'Intensive study'], family = binomial)
+summary(fm)
+
+fm = glm(anyEPY ~ initiation_st, data = ds[data_type == 'Other data'], family = binomial)
+summary(fm)
 
 
 # Figure 2b
